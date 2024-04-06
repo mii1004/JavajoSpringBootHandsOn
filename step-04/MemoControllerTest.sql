e0998292b6c8fcd39ef0e1c8341d7cc4197126b6
mii1004 <miyuki.m1004@gmail.com>
2024-04-06 16:37:08 +0900

    テストデータを修正

--- step-04/MemoControllerTest.sql
+++ step-04/MemoControllerTest.sql
@@ -1,8 +1,8 @@
 DELETE FROM `memo`;
 DELETE FROM `user`;
 
-INSERT INTO `user` (id, name, email_address) VALUES (1, 'User1', 'user1@example.com');
-INSERT INTO `user` (id, name, email_address) VALUES (2, 'User2', 'user2@example.com');
+INSERT INTO `user` (id, name, email_address) VALUES (1, 'tanaka', 'tanaka@example.com');
+INSERT INTO `user` (id, name, email_address) VALUES (2, 'sato', 'sato@example.com');
 
-INSERT INTO `memo` (id, body, user_id) VALUES (1, 'First memo', 1);
-INSERT INTO `memo` (id, body, user_id) VALUES (2, 'Second memo', 2);
+INSERT INTO `memo` (id, body, user_id) VALUES (1, 'takoyaki', 1);
+INSERT INTO `memo` (id, body, user_id) VALUES (2, 'okonomiyaki', 2);
